d2944896246235377b118a2de5128e656d88b81e
Thomas Holmes <thomasholmes@outlook.com>
2025-02-16 15:13:45 +0000

    Slightly better map rendering.

--- app/main.gd
+++ app/main.gd
@@ -13,20 +13,34 @@ func _on_request_completed(result, response_code, headers, body):
     var lon_cumm = 0.0
     var coord_count = 0
     for way in ways:
+        var vector_list: Array[Vector3] = []
+        var vector_2d_list: Array[Vector2] = []
         for c in way.coordinates:
-            if coord_count > 2000:
-                break
+            #if coord_count > 2000:
+            #    break
             var lat = (c.lat - 50) * 1000
             var lon = c.lon * 1000
+            
+            vector_list.append(Vector3(lat, 0.0, lon))
+            vector_2d_list.append(Vector2(lat, lon))
             
             lat_cumm += lat
             lon_cumm += lon
             coord_count += 1
-            
-            var instance: Node3D = scene.instantiate()
-            $map.add_child(instance)
-            instance.position.x = lat
-            instance.position.z = lon
+        if way.closedLoop:
+            if vector_2d_list.size() == 0:
+                printerr("Way %s: %s, has no coordinates." % [way.id, way.name])
+                continue;
+            var mesh = Draw3D.triangulate_polygon(vector_2d_list)
+            if mesh != null:
+                $map.add_child(mesh)
+        else:
+            if vector_list.size() == 0:
+                printerr("Way %s: %s, has no coordinates." % [way.id, way.name])
+                continue;
+            var draw_3d = Draw3D.new()
+            $map.add_child(draw_3d)
+                
     var avg_lat = lat_cumm / coord_count
     var avg_lon = lon_cumm / coord_count
     $Camera3D.position.x = avg_lat
@@ -37,8 +51,18 @@ func _on_request_completed(result, response_code, headers, body):
     print("response processed")
 
 func _process(delta: float) -> void:
-    if Input.is_action_just_pressed("ui_up"):
-        $Camera3D.position.y += 1.0
-    if Input.is_action_just_pressed("ui_down"):
-        $Camera3D.position.y -= 1.0
+    var zoom_factor := 0.1
+    var move_factor := 0.1
+    if Input.is_action_pressed("ui_up"):
+        $Camera3D.position.y -= zoom_factor
+    if Input.is_action_pressed("ui_down"):
+        $Camera3D.position.y += zoom_factor
+    if Input.is_action_pressed("left"):
+        $Camera3D.position.x += move_factor
+    if Input.is_action_pressed("right"):
+        $Camera3D.position.x -= move_factor
+    if Input.is_action_pressed("up"):
+        $Camera3D.position.z += move_factor
+    if Input.is_action_pressed("down"):
+        $Camera3D.position.z -= move_factor
         

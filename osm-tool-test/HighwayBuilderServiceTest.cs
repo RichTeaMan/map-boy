6c93f4b6ffe6c2060bc24cea55b172bbe8abb758
Thomas Holmes <thomasholmes@outlook.com>
2025-02-28 18:38:58 +0000

    Fixed incorrect highway points. Added unit test.

---
+++ osm-tool-test/HighwayBuilderServiceTest.cs
@@ -0,0 +1,27 @@
+﻿namespace OsmTool.Test;
+
+[TestClass]
+public sealed class HighwayBuilderServiceTest
+{
+    [TestMethod]
+    public void BasicTest()
+    {
+        var highwayBuilderService = new HighwayBuilderService();
+        var coords = new Coord[] {
+            new Coord(1,0),
+            new Coord(1.5,0),
+            new Coord(2,0)
+        };
+        var highwayCoords = highwayBuilderService.CalcHighwayCoordinates(coords, 0.1).ToArray();
+
+        var expectedCoords = new Coord[] {
+            new Coord { Lat = 1, Lon = -0.1 },
+            new Coord { Lat = 1.5, Lon = -0.1 },
+            new Coord { Lat = 2, Lon = -0.1 },
+            new Coord { Lat = 2, Lon = 0.1 },
+            new Coord { Lat = 1.5, Lon = 0.1 },
+            new Coord { Lat = 1, Lon = 0.1 },
+        };
+        CollectionAssert.AreEqual(expectedCoords, highwayCoords);
+    }
+}
--- osm-tool/HighwayBuilderService.cs
+++ osm-tool/HighwayBuilderService.cs
@@ -5,9 +5,7 @@ public class HighwayBuilderService
 {
     public IEnumerable<Coord> CalcHighwayCoordinates(Coord start, Coord end, double width)
     {
-        // calculate normal angle between start and end
         double angle = Math.Atan2(end.Lon - start.Lon, end.Lat - start.Lat);
-        //Console.WriteLine($"Angle: { angle * (180 / Math.PI)}");
 
         var lat_diff = width * Math.Cos(angle + (Math.PI / 2.0));
         var lon_diff = width * Math.Sin(angle + (Math.PI / 2.0));
@@ -21,31 +19,25 @@ public class HighwayBuilderService
 
     public IEnumerable<Coord> CalcHighwayCoordinates(IEnumerable<Coord> coords, double width)
     {
-
         if (coords.Count() < 2)
         {
             throw new ArgumentException("At least two coordinates are required to calculate a highway");
         }
         var backCoords = new List<Coord>();
-        Coord? first = null;
         var current = coords.First();
-
-        //CalcHighwayCoordinates(start, )
-        //var stepCoords = CalcHighwayCoordinates(coords[0], coords[1], width).ToArray();
 
         bool firstLoop = true;
 
         foreach (var next in coords.Skip(1))
         {
             var stepCoords = CalcHighwayCoordinates(current, next, width).ToArray();
-            backCoords.Add(stepCoords[2]);
             if (firstLoop)
             {
-                first = stepCoords[0];
                 yield return stepCoords[0];
-                backCoords.Add(stepCoords[3]);
+                backCoords.Add(stepCoords[2]);
             }
             yield return stepCoords[1];
+            backCoords.Add(stepCoords[3]);
 
             firstLoop = false;
             current = next;
@@ -53,8 +45,7 @@ public class HighwayBuilderService
 
         foreach (var coord in backCoords.Reverse<Coord>())
         {
-            yield return coord;
+           yield return coord;
         }
-        yield return first!;
     }
 }

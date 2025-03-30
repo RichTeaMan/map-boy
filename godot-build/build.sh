cf3378615b3e6cbc0fe440e044059148a56b6bbd
Thomas Holmes <thomasholmes@outlook.com>
2025-03-30 19:31:19 +0100

    Platforms now built to different directories.

--- godot-build/build.sh
+++ godot-build/build.sh
@@ -7,7 +7,7 @@ docker build -t godot-build .
 
 mkdir -p bin
 
-test -t 1 && USE_TTY="-t" # check if TTY is available
+test -t 1 && USE_TTY="-it" # check if TTY is available
 docker run --rm ${USE_TTY} --user $(id -u):$(id -g) -v $(pwd)/bin:/project/bin godot-build
 
 chown -R $(id -u):$(id -g) ./bin
--- godot-build/docker-entrypoint.sh
+++ godot-build/docker-entrypoint.sh
@@ -2,8 +2,6 @@
 
 set -e
 
-mkdir bin -p
-
 git clone --branch "4.4-stable" --depth 1 https://github.com/godotengine/godot.git
 
 cd godot
@@ -11,10 +9,20 @@ cd godot
 /project/emsdk/emsdk activate latest
 source /project/emsdk/emsdk_env.sh
 
-mkdir bin
-scons platform=linuxbsd float=64 precision=double production=yes
-scons platform=windows float=64 precision=double production=yes
-scons platform=web target=template_release precision=double threads=no
-scons platform=web target=template_debug precision=double threads=no
+mkdir -p bin
 
-mv bin/* ../bin/.
+mkdir ../bin/linuxbsd
+scons platform=linuxbsd float=64 precision=double production=yes
+mv bin/* ../bin/linuxbsd/.
+
+mkdir ../bin/windows
+scons platform=windows float=64 precision=double production=yes
+mv bin/* ../bin/windows/.
+
+mkdir ../bin/web-release-no-threads
+scons platform=web target=template_release precision=double threads=no
+mv bin/* ../bin/web-release-no-threads/.
+
+mkdir ../bin/web-debug-no-threads
+scons platform=web target=template_debug precision=double threads=no
+mv bin/* ../bin/web-debug-no-threads/.

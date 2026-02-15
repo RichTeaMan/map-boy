554088d2b54500aff5f49717de56657991b48771
Thomas Holmes <thomasholmes@outlook.com>
2026-02-15 15:04:15 +0000

    Added 302 following for data fetch scripts.

--- download-data.sh
+++ download-data.sh
@@ -5,12 +5,12 @@ MAP_NAME="${MAP_NAME,,}" # lowercases. works in bash 4.0
 
 if [[ "$MAP_NAME" = "greater-london" ]]; then
     echo "Downloading $MAP_NAME..."
-    curl https://download.geofabrik.de/europe/united-kingdom/england/greater-london-latest.osm.pbf -o greater-london.osm.pbf
+    curl -L https://download.geofabrik.de/europe/united-kingdom/england/greater-london-latest.osm.pbf -o greater-london.osm.pbf
 elif [[ "$MAP_NAME" = "england" ]]; then
     echo "Downloading $MAP_NAME..."
-    curl https://download.geofabrik.de/europe/united-kingdom/england-latest.osm.pbf -o england.osm.pbf
+    curl -L https://download.geofabrik.de/europe/united-kingdom/england-latest.osm.pbf -o england.osm.pbf
 else
-    echo "Unknown map name '$MAP_NAME'."
+    echo "Unknown map name '$MAP_NAME'. Try 'greater-london' or 'england'."
     exit 1
 fi
 
